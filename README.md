6a2a3c466459bd43f87bc7c4ae6ff37153587e75
JPMendieta <jpmendietac@ut.edu.co>
2023-11-03 12:38:52 -0500

    Updates

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ This repository contains a technical test that uses the Django Rest framework to
 2. **Filters for the Endpoint:**
    - The endpoint includes up to 3 filtering options, allowing users to refine the results from the consumed API. Filters may include, for example, a character's name, species, and status.
 
+3. **Option to Download Data in a ZIP File:**
+   - We offer users the ability to download the information in a ZIP file that contains the data in JSON format. This allows users to store the data locally and use it as needed.
 
 This project is designed in a modular and extensible way, allowing developers to add new features and consume different public APIs in the future. Good practices in Django Rest Framework development have been followed, and clear documentation for its use is provided.
 
@@ -20,7 +22,27 @@ This project is designed in a modular and extensible way, allowing developers to
 5. Start the development server with `python manage.py runserver`.
 6. Access the API via the URL provided by the development server.
 
+### Filtering Data
+To filter the data from the API or the database, you can use the following query parameters in the URL:
+
+- `species`: Filter characters by species.
+- `status`: Filter characters by status.
+
+Example: To filter characters by species (e.g., "Human") and status (e.g., "Alive"), use the following URL:
+
+http://localhost:8000/characters/?species=Human&status=Alive
+
+
+### Downloading Data
+To download the data in a ZIP file, add the `download` parameter to the URL:
+
+- `download=true`: Download data in a ZIP file.
+
+Example: To download the filtered data as a ZIP file, use the following URL:
+
+http://localhost:8000/characters/?species=Human&status=Alive&download=true
+
+
 If not all points were completed, the technical test is in the state where it was stopped, and additional features can be added as needed.
 
 Thank you for reviewing this technical test! If you have any questions or suggestions, please feel free to get in touch.
-
--- data_api/views.py
+++ data_api/views.py
@@ -1,13 +1,13 @@
+import zipfile
+from io import BytesIO
+import json
+import requests
+from django.http import HttpResponse
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
-import requests
-import zipfile
-from django.http import HttpResponse
-from io import BytesIO
 from .models import Character
 from .serializers import CharacterSerializer
-
 
 class AllCharacters(APIView):
     def get(self, request):
@@ -19,7 +19,6 @@ class AllCharacters(APIView):
             return Response(data)
         else:
             return Response({"error": "Failed to fetch data from the API."}, status=response.status_code)
-
 
 class CharacterList(generics.ListAPIView):
     serializer_class = CharacterSerializer
@@ -55,21 +54,30 @@ class CharacterList(generics.ListAPIView):
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
         serializer = CharacterSerializer(queryset, many=True)
+
+        if request.query_params.get('download'):
+            # Prepara los datos para la descarga
+            characters_data = serializer.data
+
+            # Crea un archivo ZIP en memoria y agrega los datos JSON
+            buffer = BytesIO()
+            with zipfile.ZipFile(buffer, 'w') as zipf:
+                # Agrega los datos de la API a un archivo JSON en el ZIP
+                api_url = "https://rickandmortyapi.com/api/character"
+                response = requests.get(api_url)
+                api_data = response.json()
+                api_data_json = json.dumps(api_data, indent=4)
+                zipf.writestr("api_data.json", api_data_json)
+
+                # Agrega los datos de la base de datos a un archivo JSON en el ZIP
+                db_data_json = json.dumps(characters_data, indent=4)
+                zipf.writestr("database_data.json", db_data_json)
+
+            # Configura la respuesta HTTP para descargar el archivo ZIP
+            buffer.seek(0)
+            response = HttpResponse(buffer.read(), content_type="application/zip")
+            response['Content-Disposition'] = 'attachment; filename="characters_data.zip"'
+
+            return response
+
         return Response(serializer.data)
-
-    def download_data_as_zip(self, request):
-        # Obtener la lista de personajes
-        self.fetch_and_store_data_from_api()
-        queryset = Character.objects.all()
-        serializer = CharacterSerializer(queryset, many=True)
-        characters_data = serializer.data
-
-        # Crear un archivo ZIP en memoria para almacenar los datos JSON
-        buffer = BytesIO()
-        with zipfile.ZipFile(buffer, 'w') as zipf:
-            zipf.writestr('characters.json', json.dumps(characters_data, indent=2))
-
-        # Crear una respuesta HTTP con el archivo ZIP
-        response = HttpResponse(buffer.getvalue(), content_type='application/zip')
-        response['Content-Disposition'] = 'attachment; filename=characters.zip'
-        return response
